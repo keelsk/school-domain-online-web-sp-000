3ea4ac370315ae283bb4c0caf75fd5ebe229b108
Kirsten Keels <keelsk@gmail.com>
2020-02-29 01:10:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,10 +5,11 @@ class School
   
   def initialize(name)
     @name = name
+    @roster = { }
   end
   
   def roster
-    @roster = { }
+    @roster
   end
   
   def add_student(student_name, grade)
